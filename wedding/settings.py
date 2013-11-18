4a9f6924f7886890a94dac9f7218e36b414b92c4
Paul Nichols <pnichols104@gmail.com>
2013-11-18 18:00:39 -0500

    updated settings for static

--- wedding/settings.py
+++ wedding/settings.py
@@ -61,21 +61,13 @@ MEDIA_ROOT = '/Users/paulnichols/code/wedding/mainsite/static/images/photoalbum/
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = 'http://127.0.0.1:8000/photos/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
