f6f525d10ce6e4f04add6fc03a3624c23eab44db
Paul Nichols <pnichols104@gmail.com>
2014-05-21 20:00:17 -0400

    updated settings

--- wedding/urls.py
+++ wedding/urls.py
@@ -28,3 +28,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+import settings
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
